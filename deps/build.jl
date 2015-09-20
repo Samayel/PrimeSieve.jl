c4e075f740e6385ac0c67c1e419a3027f61f1777
Markus John <johm@quyo.de>
2015-09-20 10:57:02 +0200

    Switch DeepConvert.jl repo

--- deps/build.jl
+++ deps/build.jl
@@ -3,7 +3,7 @@ using BinDeps
 try
     eval(Expr(:import,:DeepConvert))
 catch err
-    Pkg.clone("https://github.com/jlapeyre/DeepConvert.jl")
+    Pkg.clone("https://github.com/Samayel/DeepConvert.jl")
     Pkg.build("DeepConvert")
 end
 
