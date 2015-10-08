635c790dda1d2ecfeec6c492d5e019303e40bfa9
Markus John <johm@quyo.de>
2015-10-08 19:41:12 +0200

    Upgrade to primecount 2.3

--- deps/build.jl
+++ deps/build.jl
@@ -36,7 +36,7 @@ deps = [
 ]
 
 provides(Sources, URI("http://dl.bintray.com/kimwalisch/primesieve/primesieve-5.4.2.tar.gz"), primesieve, os = :Unix)
-provides(Sources, URI("http://dl.bintray.com/kimwalisch/primecount/primecount-2.2.tar.gz"), primecount, os = :Unix)
+provides(Sources, URI("http://dl.bintray.com/kimwalisch/primecount/primecount-2.3.tar.gz"), primecount, os = :Unix)
 provides(Sources, URI("https://gforge.inria.fr/frs/download.php/file/32159/ecm-6.4.4.tar.gz"), gmpecm, os = :Unix)
 # Getting zip- or tarball from github with a predictable name is mysterious to me.
 # But, pushing tags allows downloading this way...
@@ -81,9 +81,9 @@ provides(SimpleBuild,
          end),smsieve, os = :Unix)
 
 if Int == Int32
-    provides(Binaries, Dict(URI("https://dl.bintray.com/samayel/julia/primesieve_deps_win32_p4_20150919204804.tar.gz") => deps), os = :Windows)
+    provides(Binaries, Dict(URI("https://dl.bintray.com/samayel/julia/primesieve_deps_win32_p4_20151008193255.tar.gz") => deps), os = :Windows)
 else
-    provides(Binaries, Dict(URI("https://dl.bintray.com/samayel/julia/primesieve_deps_win64_k8-sse3_20150919204500.tar.gz") => deps), os = :Windows)
+    provides(Binaries, Dict(URI("https://dl.bintray.com/samayel/julia/primesieve_deps_win64_k8-sse3_20151008193044.tar.gz") => deps), os = :Windows)
 end
 
 isdir(joinpath(pkgdir, "deps", "usr")) && rm(joinpath(pkgdir, "deps", "usr"), recursive=true)
