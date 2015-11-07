7742249eaec6f2ccd13101ce9f2fff891a0888e0
Markus John <johm@quyo.de>
2015-11-07 16:38:00 +0100

    Fix type stability

--- src/msieve.jl
+++ src/msieve.jl
@@ -2,34 +2,28 @@ export mfactor
 
 const smsievelib = @windows ? "libsmsieve.dll" : "libsmsieve.so"
 
-type Msieveopts
-    n::AbstractString
+type Msieveopts{S}
+    n::ASCIIString
     deadline::Int
-    logfile::AbstractString
-    deepecm::Bool
+    logfile::S
+    ecm::Bool
     info::Bool    
 end
 
 # Send the string to msieve and return c struct msieve_obj
 function runmsieve(opts::Msieveopts)
-    n = opts.n
-    d = opts.deadline
-    logfile = opts.logfile
-    deepecm = opts.deepecm
     numcores = 1
-    ecmflag = deepecm ? 1 : 0
-    infoflag = opts.info ? 1 : 0    
+
+    logfile = opts.logfile
+    ecm = opts.ecm ? 1 : 0
+    info = opts.info ? 1 : 0
+
     res = try
-        if (logfile == "")
-            ccall((:factor_from_string,smsievelib), Ptr{Void}, (Ptr{UInt8},Int,Int,Ptr{UInt8},Int,Int),
-                  n, numcores, d, C_NULL, ecmflag, infoflag)
-        else
-            ccall((:factor_from_string,smsievelib), Ptr{Void}, (Ptr{UInt8},Int,Int,Ptr{UInt8},Int, Int),
-                  n, numcores, d, logfile, ecmflag, infoflag)
-        end
+        ccall((:factor_from_string,smsievelib), Ptr{Void}, (Ptr{UInt8},Int,Int,Ptr{UInt8},Int,Int), opts.n, numcores, opts.deadline, logfile == "" ? C_NULL : logfile, ecm, info)
     catch
         error("factor_from_string failed")
     end
+
     res == C_NULL && throw(InterruptException())
     res
 end
@@ -44,21 +38,22 @@ msieve_free(obj) =  ccall((:msieve_obj_free_2,smsievelib), Void, (Ptr{Void},), o
 # A ptr to next struct factor, correponding to next factor, is returned.
 function get_one_factor_value(factor)
     a = Array(UInt8,500) # max num digits input to msieve is 300
-    nextfactor = ccall((:get_one_factor_value,smsievelib), Ptr{Void}, (Ptr{Void},Ptr{UInt8},Int),
-                       factor,a,length(a))
-    return(nextfactor,bytestring(convert(Ptr{UInt8},pointer(a))))
+    nextfactor = ccall((:get_one_factor_value,smsievelib), Ptr{Void}, (Ptr{Void},Ptr{UInt8},Int), factor, a, length(a))
+
+    nextfactor, bytestring(convert(Ptr{UInt8}, pointer(a)))
 end
 
 # Send ptr to first struct factor. Return all factors as array of strings 
 function get_all_factor_values(factor)
-    allf = Array(AbstractString,0)
+    allf = Array(AbstractString, 0)
     nfactor = factor
-    n = get_num_factors(factor)
-    for i in 1:n
-        (nfactor,sfact) = get_one_factor_value(nfactor)
-        push!(allf,sfact)
+
+    for i in 1:get_num_factors(factor)
+        nfactor, sfact = get_one_factor_value(nfactor)
+        push!(allf, sfact)
     end
-    return allf
+
+    allf
 end
 
 # Send n as string to msieve, return all factors as array of strings
@@ -70,43 +65,44 @@ function runallmsieve(opts::Msieveopts)
 end
 
 # input factors as Array of strings. Output Array of Integers (Usually of type Int)
-function factor_strings_to_integers(sfactors::Array{AbstractString})
-    m = length(sfactors)
-    n1 = eval(parse(sfactors[m]))
-    T = typeof(n1)
-    arr = Array(T,m)
-    arr[m] = n1
-    @inbounds for i in 1:m-1
-        arr[i] = parse(T,sfactors[i])
+function factor_strings_to_integers{T<:Integer}(::Type{T}, sfactors::Array{AbstractString})
+    arr = Array(T, length(sfactors))
+    @inbounds for (i, f) in enumerate(sfactors)
+        arr[i] = parse(T, f)
     end
     arr    
 end
 
 # Send string to msieve. Return factors as list of Integers.
-mfactorl(opts::Msieveopts) = factor_strings_to_integers(runallmsieve(opts))
+mfactorl{T<:Integer}(::Type{T}, opts::Msieveopts) = factor_strings_to_integers(T, runallmsieve(opts))
 
 # Send string to msieve. Return factors in Dict, like Base.factor
-function mfactor(opts::Msieveopts)
-    arr = mfactorl(opts)
-    T = eltype(arr)
+function mfactor{T<:Integer}(::Type{T}, opts::Msieveopts)
+    arr = mfactorl(T, opts)
     d = Dict{T,Int}()
-    @inbounds for i in arr d[i] = get(d,i,0) + 1 end
+    @inbounds for i in arr
+        d[i] = get(d, i, 0) + 1
+    end
     d
 end
 
-function mfactor(x::Union{AbstractString,Integer}; deadline::Integer = 0, logfile::AbstractString = "", ecm::Bool = false,
-                 info::Bool = false)
-    mfactor(Msieveopts(string(x),deadline,logfile,ecm,info))
+function mfactor(x::AbstractString; deadline::Integer = 0, logfile::AbstractString = "", ecm::Bool = false, info::Bool = false)
+    opts = Msieveopts(x, deadline, logfile, ecm, info)
+    mfactor(BigInt, opts)
+end
+
+function mfactor{T<:Integer}(x::T; deadline::Integer = 0, logfile::AbstractString = "", ecm::Bool = false, info::Bool = false)
+    opts = Msieveopts(string(x), deadline, logfile, ecm, info)
+    mfactor(T, opts)
 end
 
 for (thetype) in ( :AbstractString, :Integer ) 
     @eval begin
-        function mfactor{T<:$thetype}(a::AbstractArray{T,1}; dl::Integer=0, logfile::AbstractString = "",
-                                      ecm::Bool = false, info::Bool = false)
-            outa = Array(Any,0)
+        function mfactor{T<:$thetype}(a::AbstractArray{T,1}; dl::Integer = 0, logfile::AbstractString = "", ecm::Bool = false, info::Bool = false)
+            outa = Array(Any, 0)
             for x in a
                 res = mfactor(x; deadline=dl, logfile=logfile, ecm=ecm, info=info)
-                push!(outa,res)
+                push!(outa, res)
             end
             outa
         end
